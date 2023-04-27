c8e095dcbdca3a8dbfd1103d7e48bf8cbcec2b0d
Omer Faruk Cetiner <omrctnr74@gmail.com>
2023-04-27 19:15:08 +0300

    Update [4]dada2_beta_diversity.R

--- [4]dada2_beta_diversity.R
+++ [4]dada2_beta_diversity.R
@@ -8,6 +8,7 @@ counts_df <- as.data.frame(ps_filtered@otu_table)
 
 metadata <- read.table("C:/Users/ASUS/Desktop/prosthesis_microbiota_study/data/metadata.txt",
                        header = TRUE, sep = "")
+
 ## create Bray-Curtis Distance
 bc_distance <- vegdist(sqrt(counts_df), method = "bray")
 bc_pcoa <- pcoa(bc_distance)
@@ -24,55 +25,48 @@ bc_pcoa$values$Relative_eig[[2]] * 100
 
 
 ## PERMANOVA Test according to the Condition was not significant
-permanova <- adonis2(bc_distance~ Condition, data=metadata,permutations = 9999)
-permanova
-p.adjust(permanova$`Pr(>F)`, method = "fdr")
+permanova_cond <- adonis2(bc_distance~ Condition, data=metadata,permutations = 9999)
+permanova_cond
+p.adjust(permanova_cond$`Pr(>F)`, method = "fdr")
 
 ## p= 0.043
 ## R= 0.025
+
+
+permanova_site <- adonis2(bc_distance~ Site, data=metadata,permutations = 9999)
+permanova_site
+
+## p= 0.33
+## R= 0.039
 
 ## PCoA Plot 
 
 pcoa_plot <- metadata %>%  
   mutate(Condition = replace(Condition, Condition == "AL", "Aseptic Loosening"),
-         Condition = replace(Condition, Condition == "PJI", "Prosthetic Joint Infection")) %>%
+         Condition = replace(Condition, Condition == "PJI", "Prosthetic Joint Infection"),
+         Site = replace(Site, Site == "Synovial_Fluid", "Synovial Fluid"),
+         Site = replace(Site, Site == "Implant", "Biofilm")) %>%
   ggplot(aes(x=pcoa1, y=pcoa2, color= Condition), fill= "white", show.legend=FALSE)+
-  geom_point() + stat_ellipse(linetype=2) + 
+  geom_point(aes(shape = Site), size = 2) + stat_ellipse(linetype=2) + 
   theme_minimal()+
   theme(axis.text.x = element_text(face = "bold",size = 9, colour = "black"),
         axis.text.y = element_text(face = "bold",size = 9, colour = "black"),
         axis.title.y = element_text(face = "bold"),
         axis.title.x = element_text(face = "bold"),
-        legend.title = element_blank(),
-        legend.position = "top",
+        legend.title = element_text(face = "bold"),
+        # legend.position = "top",
+        legend.box = "vertical",
         legend.key.size = unit(20,"pt"),
         legend.key = element_rect(fill = NA, color = NA),
         legend.text = element_text(face="bold"),
         plot.caption = element_markdown(face="bold", size = 11),
-        panel.border = element_rect(colour = "black", fill = NA,size=1))+
+        panel.border = element_rect(colour = "black", fill = NA,size=1)
+        ) +
   scale_color_manual(breaks = c("Aseptic Loosening","Prosthetic Joint Infection"),values=c("blue", "red"))+
-  labs(x = "PC1 [8.4%]", y= "PC2 [7.6%]",
-       # caption = "<i>p</i><0.05, PERMANOVA"
-  )
+  labs(x = "PC1 [8.4%]", y= "PC2 [7.6%]")+
+  guides(color = guide_legend(title = "Diagnosis"),
+         shape = guide_legend(title = "Sample Collection Site"))
 
 pcoa_plot
 
-ggsave("pcoa_plot_gg.png",plot=pcoa_plot, height=4.5, width=4.5)  
-
-
-## ## ## ## ##  
-## 3D PCoA  ##
-## ## ## ## ## 
-library(rgl)
-metadata_3d<-metadata %>%
-  mutate(condition_color= case_when(Condition == "AL" ~ "blue",
-                                    Condition == "PJI" ~ "red",
-                                    TRUE ~ NA_character_))
-
-plot3d(x=metadata_3d$pcoa1, y=metadata_3d$pcoa2, z= metadata_3d$pcoa3,
-       xlab = "PC1", ylab = "PC2", zlab = "PC3",
-       col= metadata_3d$condition_color, type = "s", size = 1)
-snapshot3d("gif/3d_pcoa_rgl.png")
-
-## the code below can be used to create an animated gif file 
-movie3d(spin3d(axis=c(0,0,1), rpm = 2), duration=10, dir="./")
+ggsave("pcoa_plot_gg.png",plot=pcoa_plot, height=4.5, width=9.5)  
