e4851be1862bb6c98b05ffb7ffb07b440fa25912
Omer Faruk Cetiner <omrctnr74@gmail.com>
2023-04-27 19:14:52 +0300

    Update [3]taxa_metrics.R

--- [3]taxa_metrics.R
+++ [3]taxa_metrics.R
@@ -13,6 +13,8 @@ tax_glom(ps_filtered, "Family")
 tax_glom(ps_filtered, "Genus")
 tax_glom(ps_filtered, "Species")
 
+a<-subset_samples(ps_phylum, Condition =="Aseptic Loosening")
+b<-subset_samples(ps_phylum, Condition =="Prosthetic Joint Infection")
 
 ps_phylum <- tax_glom(ps_filtered, "Phylum")
 taxa_names(ps_phylum) <- tax_table(ps_phylum)[, 2]
@@ -42,12 +44,12 @@ sort(taxa_sums(ps_sort_Genus),decreasing = TRUE)[4] / sum(taxa_sums(ps_sort_Genu
 sort(taxa_sums(ps_sort_Genus),decreasing = TRUE)[5] / sum(taxa_sums(ps_sort_Genus)) * 100
 
 #Species
-# ps objesinde varolan specieslerin gruplara göre ayrimi
+# ps objesinde varolan specieslerin gruplara gÃ¶re ayrimi
 
 ps_al<- subset_samples(ps_filtered, Condition == "Aseptic Loosening")
 ps_pji<- subset_samples(ps_filtered, Condition == "Prosthetic Joint Infection")
 
-# gruplarda var olan spesifik türlerin bir araya getirilip isimlerinin ASV'ler ile degistirilmesi 
+# gruplarda var olan spesifik tÃ¼rlerin bir araya getirilip isimlerinin ASV'ler ile degistirilmesi 
 ps_sort_Species_al <- tax_glom(ps_al, "Species")
 taxa_names(ps_sort_Species_al) <- tax_table(ps_sort_Species_al)[, 7]
 
@@ -66,7 +68,7 @@ colnames(pji_species_count)[2] <- "pji_read_count"
 
 all_species_count <- full_join(al_species_count, pji_species_count, by = "Species")
 
-# green geneste specieslerin önünde cins ismi olmadigi için
+# green geneste specieslerin Ã¶nÃ¼nde cins ismi olmadigi iÃ§in
 # cins isimlerini belirleyip birlestirmek gerekecek
 
 ps_specific_species <- subset_taxa(ps_filtered, Species %in% taxa_names(ps_sort_Species_al))
@@ -78,7 +80,7 @@ specific_species_with_genus <- as.data.frame(tax_table(ps_specific_species)[,6:7
 final_species <- full_join(all_species_count, specific_species_with_genus, by = "Species") %>%
   unite("full_taxa", c("Genus", "Species"),sep = " ")
 
-# total sayilari ve yüzdeleri hesapla
+# total sayilari ve yÃ¼zdeleri hesapla
 final_species <- final_species %>%
   arrange(desc(pji_read_count))%>%
   mutate(total = al_read_count + pji_read_count,
@@ -88,7 +90,7 @@ final_species <- final_species %>%
          pji_read_count_perc =  paste(" (% ",substr(pji_percentage,1,4), ")",sep = ""))%>%
   select(-al_percentage, -pji_percentage)
 
-# excelde düzenlenmesi için extract edildi
+# excelde dÃ¼zenlenmesi iÃ§in extract edildi
 writexl::write_xlsx(final_species, "final_species_table1.xlsx")
 
 # most species by diagnosis group
@@ -125,4 +127,24 @@ sort(taxa_sums(ps_sort_Species_al),decreasing = TRUE)[5] / sum(taxa_sums(ps_sort
 #   kbl() %>%
 #   kable_paper()
 # # add_header_above(c(" " = 1, "Read Counts" = 3, " "= 2))
+
+
+### extralar
+
+
+mergedGP <- merge_samples(ps_filtered, "Condition")
+
+# Use psmelt to obtain a long-format data.frame
+phy <- mergedGP %>% tax_glom(taxrank = "Phylum")%>% 
+  # transform_sample_counts(function(x)100* x / sum(x)) %>% 
+  psmelt()
+ocean <- subset(phy, Sample == "Aseptic Loosening")
+ocean2 <- subset(phy, Sample == "Prosthetic Joint Infection")
+
+
+phy
+
+ps_phylum <- tax_glom(mergedGP, "Phylum")
+
+sort(taxa_sums(ps_phylum),decreasing = TRUE)
 
